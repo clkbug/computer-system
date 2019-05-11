82945e17417f4b0a12f39324a47068920f12b67b
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2019-05-11 12:51:59 +0900

    clang-format

--- 02/02_hw.c
+++ 02/02_hw.c
@@ -1,7 +1,7 @@
 #include "02.h"
 
 // 02.55
-void hw02_55(){
+void hw02_55() {
   int a = 0x12345678;
   show_bytes((byte_pointer)&a, sizeof(int));
   // little endian  <=> 78 56 34 12
@@ -11,23 +11,23 @@ void hw02_55(){
 // 02.56 - 57 skip
 
 // 02.58
-int is_little_endian(){
+int is_little_endian() {
   int a = 0x12345678;
   char* p = (char*)&a;
-  if(*p == 0x12) {
+  if (*p == 0x12) {
     return 0;
   } else {
     return 1;
   }
 }
-  
-void hw02_58(){
-  if (is_little_endian() == 1){
+
+void hw02_58() {
+  if (is_little_endian() == 1) {
     printf("02.58... little endian\n");
   }
 }
 
-int main(){
+int main() {
   hw02_55();
 
   hw02_58();
