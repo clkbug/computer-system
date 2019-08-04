a0ec513ab6f478751217cafde48aead8dc95805c
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2019-08-04 14:28:59 +0900

    Add 08.16

--- 08/csapp.h
+++ 08/csapp.h
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 __pid_t
@@ -14,5 +16,16 @@ Fork(void)
         exit(1);
     }
 
+    return pid;
+}
+
+pid_t
+Wait(int* status)
+{
+    pid_t pid = wait(status);
+    if (pid == -1) {
+        perror("wait");
+        exit(1);
+    }
     return pid;
 }
